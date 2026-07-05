5296da19415983d3f4778292cad107c0e7f6acc9
mitmahi <mahimittal0407@gmail.com>
2026-07-06 04:38:24 +0530

    Add week 1 assignment

---
+++ raw_terminal.c
@@ -0,0 +1,130 @@
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <errno.h>
+
+
+struct termios originalTerminal;
+
+
+void die(const char *message) {
+
+    perror(message);
+    exit(1);
+
+}
+
+
+void disableRawMode() {
+
+    if (tcsetattr(
+        STDIN_FILENO,
+        TCSAFLUSH,
+        &originalTerminal
+    ) == -1) {
+
+        die("tcsetattr");
+
+    }
+
+}
+
+
+void enableRawMode() {
+
+
+    if (tcgetattr(
+        STDIN_FILENO,
+        &originalTerminal
+    ) == -1) {
+
+        die("tcgetattr");
+
+    }
+
+
+    atexit(disableRawMode);
+
+
+    struct termios raw = originalTerminal;
+
+
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+
+    raw.c_oflag &= ~(OPOST);
+
+    raw.c_cflag |= (CS8);
+
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+
+
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+
+    if (tcsetattr(
+        STDIN_FILENO,
+        TCSAFLUSH,
+        &raw
+    ) == -1) {
+
+        die("tcsetattr");
+
+    }
+
+}
+
+
+int main() {
+
+
+    enableRawMode();
+
+
+    while (1) {
+
+
+        char c = '\0';
+
+
+        if (
+            read(STDIN_FILENO, &c, 1) == -1
+            && errno != EAGAIN
+        ) {
+
+            die("read");
+
+        }
+
+
+        if (iscntrl(c)) {
+
+            printf("%d\r\n", c);
+
+        }
+
+        else {
+
+            printf(
+                "%d ('%c')\r\n",
+                c,
+                c
+            );
+
+        }
+
+
+        if (c == 'q') {
+
+            break;
+
+        }
+
+    }
+
+
+    return 0;
+
+}
